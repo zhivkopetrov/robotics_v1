0ef98f4a413e06a26c6b12282a5366d5d19bf0f6
zhivko.petrov <zhivko.petrov@ocado.com>
2022-02-07 22:17:05 +0200

    test: experiment with service server

--- src/robo_collector/robo_collector_gui/src/external_api/ControllerExternalBridge.cpp
+++ src/robo_collector/robo_collector_gui/src/external_api/ControllerExternalBridge.cpp
@@ -12,6 +12,7 @@
 //Own components headers
 
 using robo_collector_interfaces::msg::RobotMoveType;
+using robo_collector_interfaces::srv::GetCurrentCoins;
 
 namespace {
 //TODO create separate message helper utility file
@@ -56,14 +57,28 @@ int32_t ControllerExternalBridge::init(
       ROBOT_MOVE_TYPE_TOPIC, QoS,
       std::bind(&ControllerExternalBridge::onMoveMsg, this, _1));
 
+  _getCoinsService = create_service<GetCurrentCoins>("getCurrentCoins",
+      std::bind(&ControllerExternalBridge::handleService, this, _1, _2));
+
   return SUCCESS;
 }
 
 void ControllerExternalBridge::onMoveMsg(const RobotMoveType::SharedPtr msg) {
   const auto moveType = getMoveType(msg);
-  const auto f = [this, moveType](){
+  const auto f = [this, moveType]() {
     _outInterface.moveButtonClickCb(moveType);
   };
 
   _outInterface.invokeActionEventCb(f, ActionEventType::NON_BLOCKING);
 }
+
+void ControllerExternalBridge::handleService(
+    [[maybe_unused]]const std::shared_ptr<GetCurrentCoins::Request> request,
+    std::shared_ptr<GetCurrentCoins::Response> response) {
+  const auto f = [this, &response](){
+    response->coins = _coins++;
+  };
+
+  _outInterface.invokeActionEventCb(f, ActionEventType::BLOCKING);
+}
+
