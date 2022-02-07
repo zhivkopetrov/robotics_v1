e4284076a46780804af7b6fdb85800fe5b6905b7
zhivko.petrov <zhivko.petrov@ocado.com>
2022-02-07 11:12:33 +0200

    feat: robo games demo initial commit

---
+++ src/robo_collector/robo_collector_gui/include/robo_collector_gui/robo_miner/RoboMinerGui.h
@@ -0,0 +1,23 @@
+#ifndef ROBO_MINER_ROBOMINERGUI_H_
+#define ROBO_MINER_ROBOMINERGUI_H_
+
+//C system headers
+
+//C++ system headers
+#include <cstdint>
+
+//Other libraries headers
+
+//Own components headers
+
+//Forward declarations
+
+class RoboMinerGui {
+public:
+
+
+private:
+
+};
+
+#endif /* ROBO_MINER_ROBOMINERGUI_H_ */
---
+++ src/robo_collector/robo_collector_gui/src/robo_miner/RoboMinerGui.cpp
@@ -0,0 +1,12 @@
+//Corresponding header
+#include "robo_collector_gui/robo_miner/RoboMinerGui.h"
+
+//C system headers
+
+//C++ system headers
+
+//Other libraries headers
+#include "utils/ErrorCode.h"
+#include "utils/Log.h"
+
+//Own components headers
