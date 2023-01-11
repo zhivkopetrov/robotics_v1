f057ef24bb58483ef4905c0ea0cb0b25bb75b623
Zhivko Petrov <zhivko.petrov@ocado.com>
2023-01-11 22:29:59 +0200

    refactor: tidy up install scripts

--- scripts/assisted_install/full_install_on_host.sh
+++ scripts/assisted_install/full_install_on_host.sh
@@ -18,3 +18,5 @@ export TZ=Etc/UTC
 
 # own dependencies (personal open-source libraries and tools)
 ./scripts/assisted_install/clone_dependencies.sh $ROS2_DISTRO
+
+./scripts/assisted_install/clean_up.sh
