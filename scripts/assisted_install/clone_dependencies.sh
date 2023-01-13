91100192d5b78d9dc6de3d5f343f0836e1433281
Zhivko Petrov <zhivko.petrov@ocado.com>
2023-01-13 16:45:06 +0200

    feat: utilise pre-build ur_driver and ursim images stored in docker hub

--- scripts/assisted_install/clone_dependencies.sh
+++ scripts/assisted_install/clone_dependencies.sh
@@ -18,3 +18,11 @@ source /opt/ros/$ROS2_DISTRO/setup.bash
 rosdep init && \
     rosdep update && \
     rosdep install --from-paths src -y --ignore-src
+
+# pull docker images from DockerHub containing:
+# - Universal Robots ROS2 driver
+# - Universal Robots Simulator (URSim) - utilising a ur5 robot
+# - Rviz2
+
+docker pull zhivkopetrov90/robotics_accelerator_2022:ursim_img
+docker pull zhivkopetrov90/robotics_accelerator_2022:driver_img
