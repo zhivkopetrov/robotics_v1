32ec9194bf83622d6c0063fa7fcc249b107b11bb
Zhivko Petrov <zhivko.petrov@ocado.com>
2023-01-14 18:06:44 +0200

    fix: ros2 installation script package name mismatch

--- scripts/assisted_install/ros2_install.sh
+++ scripts/assisted_install/ros2_install.sh
@@ -26,29 +26,17 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
     tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 apt update && apt install -y \
-  python3-flake8-docstrings \
   python3-pip \
-  python3-pytest-cov \
+  python3-argcomplete \
   ros-dev-tools
 
 apt update && apt install -y \
-  python3-flake8-blind-except \
-  python3-flake8-builtins \
-  python3-flake8-class-newline \
-  python3-flake8-comprehensions \
-  python3-flake8-deprecated \
-  python3-flake8-import-order \
-  python3-flake8-quotes \
-  python3-pytest-repeat \
-  python3-pytest-rerunfailures \
-  python3-colcon-common-extensions \
-  python3-vcstool
-
-apt update && apt install -y \
-  ros-$ROS2_DISTRO-desktop-full \
+  ros-$ROS2_DISTRO-desktop \
   ros-$ROS2_DISTRO-ros2cli \
   ros-$ROS2_DISTRO-ros2controlcli \
   ros-$ROS2_DISTRO-ros2cli-common-extensions \
   ros-$ROS2_DISTRO-ros2bag \
   ros-$ROS2_DISTRO-joint-trajectory-controller \
-  ros-$ROS2_DISTRO-ur-msgs
+  ros-$ROS2_DISTRO-ur-msgs \
+  python3-colcon-common-extensions \
+  python3-vcstool
