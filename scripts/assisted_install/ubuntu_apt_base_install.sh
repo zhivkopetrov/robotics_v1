4cac815ffa8d3d76da54e4fbbcf5d8a47752633f
Zhivko Petrov <zhivko.petrov@ocado.com>
2023-01-14 21:16:46 +0200

    fix: remove ubuntu22.04 specific fix for clang

--- scripts/assisted_install/ubuntu_apt_base_install.sh
+++ scripts/assisted_install/ubuntu_apt_base_install.sh
@@ -4,9 +4,7 @@
 # default clang/clang++ is 14, which requires gcc/g++-12 libs
 apt update && apt-get install -y \
     build-essential \
-    clang \
-    gcc-12 \
-    g++-12
+    clang
 
 # install utilities
 apt update && apt-get install -y \
