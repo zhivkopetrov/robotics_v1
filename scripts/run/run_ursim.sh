417032e00cea875d7cd081da3a66e2d7c04ee23e
Zhivko Petrov <zhivko.petrov@ocado.com>
2023-02-09 23:29:05 +0200

    feat: implement ursim & ur_driver preset run scripts

---
+++ scripts/run/run_ursim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SCRIPT_NAME=`basename "$0"`
+ROBOT_TYPE=ur10e
+
+echo "Processing $SCRIPT_NAME"
+
+if [ -z "$1" ]; then
+    echo "No argument supplied for ROBOT_TYPE. Assuming '$ROBOT_TYPE'"
+else
+    ROBOT_TYPE=$1
+    echo "Using ROBOT_TYPE=$ROBOT_TYPE"
+fi
+
+source install/setup.bash
+
+ros2 run ur_robot_driver start_ursim.sh -m $ROBOT_TYPE
